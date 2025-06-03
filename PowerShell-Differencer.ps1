1a89fc981c4d3735a8366d1760eb9f968df97c69
rulesilol <andrewlei323@gmail.com>
2025-06-03 22:14:17 +1000

    Created the local version

--- PowerShell-Differencer.ps1
+++ PowerShell-Differencer.ps1
@@ -19,39 +19,56 @@ foreach ($computer in $ComputerName) {
         [void]$List.Add($currentLine)
     }
 
+    $ListeningPorts = Get-NetTCPConnection -State Listen | Select-Object LocalPort, OwningProcess
+    foreach($ListeningPort in $ListeningPorts) {
+        $currentLine = @("Listening Port", $ListeningPort.LocalPort)
+        [void]$List.Add($currentLine)
+    }
+
+    $LocalUsers = 
+
     $rootBase.Add($computer, $List)
 }
 
 Write-Host "Initial Baseline Complete"
+Write-Host "Time, Computer, Type, Information, Comment"
 
 
 while ($switch -eq $True) {
     foreach ($computer in $ComputerName) {
-        $NewList = [System.Collections.ArrayList]@()
+        $List = [System.Collections.ArrayList]@()
         
         $ScheduledTasks = Get-ScheduledTask | Select-Object TaskName
         foreach($scheduledTask in $ScheduledTasks) {
             $currentLine = @("Scheduled Task", $scheduledTask.TaskName)
-            [void]$NewList.Add($currentLine)
+            [void]$List.Add($currentLine)
         }
 
         $Services = Get-Service | Select-Object -Property Name
         foreach($service in $Services) {
             $currentLine = @("Service", $service.Name)
-            [void]$NewList.Add($currentLine)
+            [void]$List.Add($currentLine)
         }
 
+        $ListeningPorts = Get-NetTCPConnection -State Listen | Select-Object LocalPort, OwningProcess
+        foreach($ListeningPort in $ListeningPorts) {
+            $currentLine = @("Listening Port", $ListeningPort.LocalPort)
+            [void]$List.Add($currentLine)
+    }
 
 
-        $Comparison = Compare-Object -DifferenceObject $NewList -ReferenceObject $rootBase[$computer]
-        $rootBase[$computer] = $NewList
+
+        $Comparison = Compare-Object -DifferenceObject $List -ReferenceObject $rootBase[$computer]
+        $rootBase[$computer] = $List
+        
         foreach ($item in $Comparison) {
             # Write-Host $item.InputObject $item.SideIndicator
             $Type = $item.InputObject[0]
+            $CurrentDateTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss.fff"
             if ($item.SideIndicator -eq "=>") {
-                Write-Host $computer $item.InputObject[0] $item.InputObject[1] "$Type was added in $computer"
+                Write-Host $CurrentDateTime $computer $item.InputObject[0] $item.InputObject[1] "A $Type was added on $computer" -Separator ", "
             } elseif ($item.SideIndicator -eq "<=") {
-                Write-Host $computer $item.InputObject[0] $item.InputObject[1] "$Type was removed $computer"
+                Write-Host $CurrentDateTime $computer $item.InputObject[0] $item.InputObject[1] "A $Type was removed on $computer" -Separator ", "
             }
         }
     }
