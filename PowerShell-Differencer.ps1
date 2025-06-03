5cf92b174013f77a46e43334623c6806c4b848b8
rulesilol <andrewlei323@gmail.com>
2025-06-03 11:09:30 +1000

    Changed the name of main file

---
+++ PowerShell-Differencer.ps1
@@ -0,0 +1,52 @@
+$ComputerName = $env:COMPUTERNAME
+$switch = $True
+
+$rootBase = @{}
+
+## Initial Baseline
+foreach ($computer in $ComputerName) {
+    $List = [System.Collections.ArrayList]@()
+    
+    $ScheduledTasks = Get-ScheduledTask | Select-Object TaskName
+    foreach($scheduledTask in $ScheduledTasks) {
+        $currentLine = @("Scheduled Task", $scheduledTask.TaskName)
+        [void]$List.Add($currentLine)
+    }
+
+    $Services = Get-Service | Select-Object -Property Name
+    foreach($service in $Services) {
+        $currentLine = @("Service", $service.Name)
+        [void]$List.Add($currentLine)
+    }
+
+    $rootBase.Add($computer, $List)
+}
+
+Write-Host "Initial Baseline Complete"
+
+
+while ($switch -eq $True) {
+    foreach ($computer in $ComputerName) {
+        $NewList = [System.Collections.ArrayList]@()
+        
+        $ScheduledTasks = Get-ScheduledTask | Select-Object TaskName
+        foreach($scheduledTask in $ScheduledTasks) {
+            $currentLine = @("Scheduled Task", $scheduledTask.TaskName)
+            [void]$NewList.Add($currentLine)
+        }
+
+        $Services = Get-Service | Select-Object -Property Name
+        foreach($service in $Services) {
+            $currentLine = @("Service", $service.Name)
+            [void]$NewList.Add($currentLine)
+        }
+
+
+
+        $Comparison = Compare-Object -DifferenceObject $NewList -ReferenceObject $rootBase[$env:COMPUTERNAME]
+        $rootBase[$computer] = $NewList
+        foreach ($item in $Comparison) {
+            Write-Host $item.InputObject $item.SideIndicator
+        }
+    }
+}
